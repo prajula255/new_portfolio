391bfb4b6e3526de5f2c34fb6ccb5cf268aad387
prajula255 <prajulasouparnika3@gmail.com>
2024-11-29 11:31:17 +0530

    icon updated

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -3,8 +3,9 @@ import React from "react";
 function Contact() {
     return (
         <div className="contact-details">
+            <i className="fas fa-handshake"></i>
             <h2>Tell me about your next project</h2>
-            <button><i class="fa-regular fa-envelope"></i> Email me</button>
+            <button><i className="fa-regular fa-envelope"></i> Email me</button>
             <button>Whatsapp</button>
         </div>
     )
