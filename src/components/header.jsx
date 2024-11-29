e3d88a019cea7efdd910a9097ac54b90781da4ef
prajula255 <prajulasouparnika3@gmail.com>
2024-11-29 10:18:59 +0530

    bootstrap installed code updated

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 
-function Header(){
-    return(
+function Header() {
+    return (
         <header className="header">
-        <div className="left-header">
-            <a href="email@example.com">your email@example.com</a>
-            <button>copy</button>
-            <button>cv</button>
-        </div>
+            <div className='d-flex justify-content-between'>
+                <div className='d-flex'>
+                    <a href="email@example.com">kawsarvy.design@gmail.com</a>
+                    <button>copy</button>
+                    <button>cv</button>
+                </div>
 
-        <div className="right-header">
-            <a href="linked">LinkedIn/</a>
-            <a href="drib">Dribble/</a>
-            <a href="insta">Instagram</a>
-        </div>
+                <div className='d-flex'>
+                    <a href="linked">LinkedIn/</a>
+                    <a href="drib">Dribble/</a>
+                    <a href="insta">Instagram</a>
+                </div>
+            </div>
+
+
         </header>
     )
 }
--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -4,6 +4,7 @@ function Introduction(){
     return(
         <div className="intro">
             <h1>Building digital products,brands,and experience.</h1>
+            
             <button>Latest shots</button>
 
         </div>
