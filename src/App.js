82ba2a8bdcb3f337958c88641ed4cab240dcd20b
prajula255 <prajulasouparnika3@gmail.com>
2024-12-02 11:58:08 +0530

    image updated

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import Client from './components/client';
 import Contact from './components/contact';
 import Header from './components/header';
 import Introduction from './components/intro';
+import Logos from './components/logos';
 import Rightscpy from './components/rpage';
 import Services from './components/services';
 
 function App() {
   return (
-    <div className="App"style={{backgroundColor:"rgba(238,237,236,255)"}}>
+    <div className="App" style={{ backgroundColor: "rgba(238,237,236,255)" }}>
 
       <Header />
       <Introduction />
+      <Logos />
       <Client />
       <Services />
       <Contact />
---
+++ src/components/logos.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+function Logos() {
+    return (
+       <div className="lo">
+        <img src="/nb.jpg" className="img-fluid" alt="Logo" />
+       </div> 
+    )
+}
+export default Logos;
