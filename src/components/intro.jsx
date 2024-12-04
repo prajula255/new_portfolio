bcb4273df55d57eb43b21fc9c0677b5f4d4023e9
prajula255 <prajulasouparnika3@gmail.com>
2024-12-04 11:01:08 +0530

    profile set

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -3,6 +3,7 @@ import React from "react";
 function Introduction() {
     return (
         <div className="intro">
+            <img src="kau.jpg" className="pimg"/>
             <h1>Building digital products,brands,and experience.</h1>
 
             <button className="btn btn-outline-secondary text-dark rounded-5">Latest shots</button>
