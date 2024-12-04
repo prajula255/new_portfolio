5ba95012a9ed3f1ced09f7e9be27039a0e9aba38
prajula255 <prajulasouparnika3@gmail.com>
2024-12-04 10:25:59 +0530

    images uploded

--- src/components/logos.jsx
+++ src/components/logos.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 function Logos() {
     return (
-       <div className="lo">
-        <img src="/nb.jpg" className="img-fluid" alt="Logo" />
-        <img src="/ccola.jpg" className="img-fluid" alt="Logo" />
-
-       </div> 
+        <div className="lo">
+            <img src="/nb.jpg" className="img-fluid" alt="Logo" />
+            <img src="/m.jpg" className="img-fluid" alt="Logo" />
+            <img src="/ccola.jpg" className="img-fluid" alt="Logo" />
+            <img src="/ad.jpg" className="img-fluid" alt="Logo" />
+            <img src="/s.jpg" className="img-fluid" alt="Logo" />
+            <img src="/ca.jpg" className="img-fluid" alt="Logo" />
+        </div>
     )
 }
 export default Logos;
