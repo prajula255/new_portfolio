d83e8ffa782cd0fe08d283a049b70d5f173f820e
prajula255 <prajulasouparnika3@gmail.com>
2024-12-05 12:11:35 +0530

    navigation bar

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -3,11 +3,13 @@ import React from "react";
 function Introduction() {
     return (
         <div className="intro">
-            <img src="kau.jpg" className="pimg"/>
+            <div className="pro">
+                <img src="kau.jpg" className="pimg" />
+                <span>Kawser Ahmed</span><br />
+
+            </div>
             <h1>Building digital products,brands,and experience.</h1>
-
             <button className="btn btn-outline-secondary text-dark rounded-5">Latest shots</button>
-
         </div>
 
     )
--- src/components/rpage.jsx
+++ src/components/rpage.jsx
@@ -4,10 +4,16 @@ function Rightscpy() {
         <div className="cpyrghts">
             <div className='d-flex justify-content-between' style={{ backgroundColor: "#f7f7f7" }}>
                 <div className='d-flex'>
-                    <h6><i className="far fa-copyright"></i>2024 All Rights Reserved.</h6>
+                    {/* <h6><i className="far fa-copyright"></i>2024 All Rights Reserved.</h6> */}
+                    <p>© 2024 All rights reserved</p>
+
+
                 </div>
                 <div className='d-flex'>
-                    <h6>LinkedIn/ Dribble/ Instagram</h6>
+                    {/* <h6>LinkedIn/ Dribble/ Instagram</h6> */}
+                    <a href="linked">LinkedIn/</a>
+                    <a href="drib">Dribble/</a>
+                    <a href="insta">Instagram</a>
                 </div>
             </div>
         </div>
